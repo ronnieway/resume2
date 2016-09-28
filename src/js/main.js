3acc50dfea2e60a65b7de0275cdeb3d4f3729bcd
Ruslan Dorozhovets <rrruslan@yahoo.com>
2016-09-28 13:19:41 +0300

    initial

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/*
+ THIRD PARTY LIBS
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/bootstrap.min.js
+
+/*
+MY CODE
+ */
+//= parts/all.js
+
+/*
+ADDITIONAL PLUGINS
+ */
+//= plugins/plugins.js
